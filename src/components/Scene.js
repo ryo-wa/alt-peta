031ac4d79b4114e762661745a40530fb5e605266
cb-ryo-watanabe <109151361+cb-ryo-watanabe@users.noreply.github.com>
2023-05-16 22:04:10 +0900

    first commit

---
+++ src/components/Scene.js
@@ -0,0 +1,12 @@
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
+
+export default function PetaScene(props) {
+    return (
+<a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
+    <a-camera gps-new-camera='gpsMinDistance: 5' rotation-reader></a-camera>
+    <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: 33.95535027392192; longitude: 130.9193714213367" scale="10 10 10"></a-entity>
+</a-scene>
+      );
+  
+}
