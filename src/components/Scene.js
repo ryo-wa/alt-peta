4a0607035ee98eca0f6acd01cab9ac0e319a724f
cb-ryo-watanabe <109151361+cb-ryo-watanabe@users.noreply.github.com>
2023-05-17 12:47:32 +0900

    changechange

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,14 +1,12 @@
-import 'aframe';
-import {Entity, Scene} from 'aframe-react';
+import 'aframe'
+import { Scene, Camera, Box } from 'aframe-react-component';
+import {Entity} from 'aframe-react';
 
 export default function PetaScene(props) {
     return (
         <Scene 
-        embedded
-        loading-screen="enabled: false;"
-        arjs="sourceType: webcam; debugUIEnabled: false;"
         >
-          <Entity primitive="a-camera" gps-camera rotation-render/>
+                      <a-camera gps-camera rotation-reader></a-camera>
           <Entity geometry={{primitive: 'box'}} material={{color: '#feb25e'}} position={{x: -2, y: 0, z: -5}}/>
           <Entity geometry={{primitive: 'box'}} material={{color: '#ff5685'}} position={{x: 2, y: 0, z: -5}} gps-new-entity-place="latitude: 33.955193; longitude: 130.919529"/>
           <Entity geometry={{primitive: 'box'}} material={{color: '#6ac7e6'}} position={{x: 0, y: 0, z: -5}}/>
