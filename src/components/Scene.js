979d289c5975ac4cc8ba0bcd0e92f3007ea79642
cb-ryo-watanabe <109151361+cb-ryo-watanabe@users.noreply.github.com>
2023-05-17 11:37:31 +0900

    fixfix11

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -8,12 +8,11 @@ export default function PetaScene(props) {
         loading-screen="enabled: false;"
         arjs="sourceType: webcam; debugUIEnabled: false;"
         >
-          <Entity primitive="a-camera" rotation-render/>
+          <Entity primitive="a-camera" gps-camera rotation-render/>
           <Entity geometry={{primitive: 'box'}} material={{color: '#feb25e'}} position={{x: -2, y: 0, z: -5}}/>
           <Entity geometry={{primitive: 'box'}} material={{color: '#ff5685'}} position={{x: 2, y: 0, z: -5}} gps-new-entity-place="latitude: 33.955193; longitude: 130.919529"/>
           <Entity geometry={{primitive: 'box'}} material={{color: '#6ac7e6'}} position={{x: 0, y: 0, z: -5}}/>
           <Entity light={{type: 'ambient'}}/>
-          <a-camera gps-camera rotation-reader></a-camera>
         </Scene>
       );
   
