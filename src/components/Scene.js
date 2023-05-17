c7d8464b36e4b374a549baba7bd9a0d6bcd873a1
cb-ryo-watanabe <109151361+cb-ryo-watanabe@users.noreply.github.com>
2023-05-17 10:50:26 +0900

    change default

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,11 +1,15 @@
 import 'aframe';
+import {Entity, Scene} from 'aframe-react';
 
 export default function PetaScene(props) {
     return (
-<a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
-    <a-camera gps-new-camera='gpsMinDistance: 5' rotation-reader></a-camera>
-    <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: 33.95535027392192; longitude: 130.9193714213367" scale="10 10 10"></a-entity>
-</a-scene>
+        <Scene>
+          <Entity primitive="a-camera" gps-camera='gpsMinDistance: 5' rotation-render/>
+          <Entity geometry={{primitive: 'box'}} material={{color: '#feb25e'}} position={{x: -2, y: 0, z: -5}}/>
+          <Entity geometry={{primitive: 'box'}} material={{color: '#ff5685'}} position={{x: 2, y: 0, z: -5}} gps-new-entity-place="latitude: 33.955193; longitude: 130.919529"/>
+          <Entity geometry={{primitive: 'box'}} material={{color: '#6ac7e6'}} position={{x: 0, y: 0, z: -5}}/>
+          <Entity light={{type: 'ambient'}}/>
+        </Scene>
       );
   
 }
